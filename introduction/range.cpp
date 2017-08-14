68a4dba5d3daa356edf5908457fda0e9365a1529
Anil Kulkarni <anil.kulkarni@outlook.com>
2017-08-14 10:03:45 -0700

    Implement a trivial solution to the problem

--- introduction/range.cpp
+++ introduction/range.cpp
@@ -1,5 +1,18 @@
 #include "./range.h"
 
+/*
+  Given a range of days, what is the max profit that can be made, given the buy and sell
+  Must be done on the open of the day
+*/
 float max_trade(const std::vector<DAY_INFO>& days) {
-  return 0.0;
+  float best_profit = 0.0;
+  for (std::vector<DAY_INFO>::size_type i = 0; i < days.size(); i++) {
+    const float buy_price = days[i].open;
+    for (std::vector<DAY_INFO>::size_type j = i; j < days.size(); j++) {
+      const float sell_price = days[j].open;
+      const float profit = sell_price - buy_price;
+      best_profit = profit > best_profit ? profit : best_profit;
+    }
+  }
+  return best_profit;
 }
--- introduction/range_unittest.cpp
+++ introduction/range_unittest.cpp
@@ -3,5 +3,37 @@
 
 TEST(RangeTest, EmptyInput) {
   const std::vector<DAY_INFO> empty;
-  EXPECT_EQ(0.0, max_trade(empty));
+  ASSERT_FLOAT_EQ(0.0, max_trade(empty));
+}
+
+TEST(RangeTest, OneDay) {
+  const std::vector<DAY_INFO> days = { { 5.2, 3.0, 8.0 } };
+  ASSERT_FLOAT_EQ(0.0, max_trade(days));
+}
+
+TEST(RangeTest, lowestFirstDay) {
+  const std::vector<DAY_INFO> days = {
+    { 5.2, 3.0, 8.0 },
+    { 6.2, 3.0, 8.0 },
+    { 7.3, 3.0, 8.0 }
+  };
+  ASSERT_FLOAT_EQ(2.1, max_trade(days));
+}
+
+TEST(RangeTest, lowestSecondDay) {
+  const std::vector<DAY_INFO> days = {
+    { 5.2, 3.0, 8.0 },
+    { 4.2, 3.0, 8.0 },
+    { 7.3, 3.0, 8.0 }
+  };
+  ASSERT_FLOAT_EQ(3.1, max_trade(days));
+}
+
+TEST(RangeTest, lowestThirdDay) {
+  const std::vector<DAY_INFO> days = {
+    { 5.2, 3.0, 8.0 },
+    { 4.2, 3.0, 8.0 },
+    { 3.3, 3.0, 8.0 }
+  };
+  ASSERT_FLOAT_EQ(0.0, max_trade(days));
 }
