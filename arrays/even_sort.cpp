8733c92f895e580e06eabe9b47ee2cd69d89da72
Anil Kulkarni <anil.kulkarni@outlook.com>
2017-08-18 12:34:35 -0700

    Implement tests for the sort array case

---
+++ arrays/even_sort.cpp
@@ -0,0 +1,8 @@
+#include "./even_sort.h"
+
+/*
+ Given an array of integers, reorder the array such that even entries appear first
+*/
+
+void even_sort(int const * const data, int length) {
+}
---
+++ arrays/even_sort.h
@@ -0,0 +1,4 @@
+#ifndef ARRAYS_EVEN_SORT_H_
+#define ARRAYS_EVEN_SORT_H_
+void even_sort(int const * const data, int length);
+#endif  // ARRAYS_EVEN_SORT_H_
